a0af74283e4036b915a31e8a4fe2a29a1035357d
Sixx <snowball2k10@hotmail.co.uk>
2015-09-22 22:41:37 +0100

    Update sh_rangers.lua

--- ss13/gamemode/schema/factions/sh_rangers.lua
+++ ss13/gamemode/schema/factions/sh_rangers.lua
@@ -1,15 +1,1 @@
-FACTION.name = "Desert Rangers"
-FACTION.desc = "A group of people who care about the people of the wastes."
-FACTION.color = Color(235, 235, 0)
-FACTION.maleModels = {
-	"models/ncr/rangercombatarmor.mdl",
-	"models/ncr/desertranger.mdl"
-}
--- Set the female models to be the same as male models.
-FACTION.femaleModels = FACTION.maleModels
-FACTION.isDefault = false
 
-
--- FACTION.index is defined when the faction is registered and is just a numeric ID.
--- Here, we create a global variable for easier reference to the ID.
-FACTION_RANGERS = FACTION.index
